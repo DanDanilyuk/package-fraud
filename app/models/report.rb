7c2cada0bd095a7212c135ac555667e0df4c033e
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-15 17:22:55 -0800

    add report validations

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,10 @@
 class Report < ApplicationRecord
+  belongs_to :address
+
+  validates :name, :presence => true
+  validates :date, :presence => true
+  validates :carrier, :presence => true
+  validates :notes, :presence => true
+  validates :code, :presence => true
+  validates :price, :presence => true
 end
