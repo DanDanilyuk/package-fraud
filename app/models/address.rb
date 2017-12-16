a6cb281bafcb3007d3f0e1ee70b293418a521c7a
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-15 17:22:43 -0800

    add address validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ApplicationRecord
+  has_many :reports
+
+  validates :street, :presence => true
+  validates :city, :presence => true
+  validates :state, :presence => true
+  validates :zip, :presence => true
 end
