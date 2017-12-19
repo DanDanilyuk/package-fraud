a1c6d33237d3f38590b3cd7f4fb34bc135570882
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-19 11:44:04 -0800

    add exceptions for failed validations on create

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -2,10 +2,10 @@ class ChecksController < ApplicationController
   def create
     standardize(checks_params)
     if @address = Address.where(street: checks_params[:street], city: checks_params[:city], state: checks_params[:state], zip: checks_params[:zip]).first
-      @address.update(orders_checked: (@address.orders_checked + 1))
+      @address.update!(orders_checked: (@address.orders_checked + 1))
       @safety = @address.risk_checker
       if @address.reports == []
-        render status: 201, json: { message: "Address is in database. safety #{@safety}",
+        render status: 200, json: { message: "Address is in database. safety #{@safety}",
                                           id: @address.id,
                                           safety_level: @safety,
                                           street: @address.street,
@@ -20,7 +20,7 @@ class ChecksController < ApplicationController
         @address.reports.each do |report|
           @reports.push(name: report.name,date: report.date,carrier: report.carrier,notes: report.notes,code: report.code,price: report.price)
         end
-        render status: 201, json: { message: "Address is in database. safety #{@safety}",
+        render status: 200, json: { message: "Address is in database. safety #{@safety}",
                                           id: @address.id,
                                           safety_level: @safety,
                                           street: @address.street,
@@ -32,7 +32,7 @@ class ChecksController < ApplicationController
                                           reports: @reports }
       end
     else
-      @address = Address.create(street: checks_params[:street], city: checks_params[:city], state: checks_params[:state], zip: checks_params[:zip])
+      @address = Address.create!(street: checks_params[:street], city: checks_params[:city], state: checks_params[:state], zip: checks_params[:zip])
       render status: 201, json: { message: "Address was not in database. It was created.",
                                   id: @address.id,
                                   safety_level: "Low-Risk",
