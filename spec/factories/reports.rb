1908c8bd6fd404e12136e0c3a9292097630b9a2c
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-19 14:34:08 -0800

    write specs for reports

--- spec/factories/reports.rb
+++ spec/factories/reports.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :report do
-    
+    name('SMOKEY THE BEAR')
+    date('2017.12.22')
+    carrier('USPS')
+    notes('Package wasnt left')
+    code(1)
+    price(199.99)
   end
 end
