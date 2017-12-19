8f5d6941ce1e396c4a265d4f6f63e69b5f118c7e
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-19 14:34:22 -0800

    write model specs for address

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Address, 'validation' do
+  it { should validate_presence_of :street }
+  it { should validate_presence_of :city }
+  it { should validate_presence_of :state }
+  it { should validate_presence_of :zip }
+  it { should validate_presence_of :orders_checked }
+  it { should validate_presence_of :risk }
+  it { should validate_uniqueness_of(:street).scoped_to([:city, :state, :zip]) }
+end
+
+describe Address, 'association' do
+  it { should have_many :reports }
+end
+
+describe Address, 'column_specification' do
+  it { should have_db_column(:street).of_type(:string) }
+  it { should have_db_column(:city).of_type(:string) }
+  it { should have_db_column(:state).of_type(:string) }
+  it { should have_db_column(:zip).of_type(:integer) }
+  it { should have_db_column(:orders_checked).of_type(:integer) }
+  it { should have_db_column(:risk).of_type(:integer) }
+  it { should have_db_column(:created_at).of_type(:datetime) }
+  it { should have_db_column(:updated_at).of_type(:datetime) }
 end
