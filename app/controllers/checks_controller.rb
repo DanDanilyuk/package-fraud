2d2177f8aaafb6073b6967f5bd5989789c3fcf36
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-19 11:06:27 -0800

    add loop through reports if they exist and return them in json

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -4,32 +4,46 @@ class ChecksController < ApplicationController
     if @address = Address.where(street: checks_params[:street], city: checks_params[:city], state: checks_params[:state], zip: checks_params[:zip]).first
       @address.update(orders_checked: (@address.orders_checked + 1))
       @safety = @address.risk_checker
-      render status: 201, json: { message: "Address is in database. safety #{@safety}",
-                                        id: @address.id,
-                                        safety_level: @safety,
-                                        street: @address.street,
-                                        city: @address.city,
-                                        state: @address.state,
-                                        zip: @address.zip,
-                                        orders_checked: @address.orders_checked,
-                                        risk: @address.risk }
+      if @address.reports == []
+        render status: 201, json: { message: "Address is in database. safety #{@safety}",
+                                          id: @address.id,
+                                          safety_level: @safety,
+                                          street: @address.street,
+                                          city: @address.city,
+                                          state: @address.state,
+                                          zip: @address.zip,
+                                          orders_checked: @address.orders_checked,
+                                          risk: @address.risk,
+                                          reports: 'No Reports' }
+      else
+        @reports = []
+        @address.reports.each do |report|
+          @reports.push(name: report.name,date: report.date,carrier: report.carrier,notes: report.notes,code: report.code,price: report.price)
+        end
+        render status: 201, json: { message: "Address is in database. safety #{@safety}",
+                                          id: @address.id,
+                                          safety_level: @safety,
+                                          street: @address.street,
+                                          city: @address.city,
+                                          state: @address.state,
+                                          zip: @address.zip,
+                                          orders_checked: @address.orders_checked,
+                                          risk: @address.risk,
+                                          reports: @reports }
+      end
     else
       @address = Address.create(street: checks_params[:street], city: checks_params[:city], state: checks_params[:state], zip: checks_params[:zip])
       render status: 201, json: { message: "Address was not in database. It was created.",
-                                        id: @address.id,
-                                        safety_level: "Safe",
-                                        street: @address.street,
-                                        city: @address.city,
-                                        state: @address.state,
-                                        zip: @address.zip,
-                                        orders_checked: @address.orders_checked,
-                                        risk: @address.risk }
+                                  id: @address.id,
+                                  safety_level: "Low-Risk",
+                                  street: @address.street,
+                                  city: @address.city,
+                                  state: @address.state,
+                                  zip: @address.zip,
+                                  orders_checked: @address.orders_checked,
+                                  risk: @address.risk,
+                                  reports: 'No Reports' }
     end
-  end
-
-  def show
-    @address = Address.find(params[:id])
-    json_response([@address,@address.reports])
   end
 
   private
