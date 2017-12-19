012da69268ce3f13d7a98291d167750cd7bacc48
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-19 15:57:02 -0800

    remove not found error - not used

--- app/controllers/concerns/error.rb
+++ app/controllers/concerns/error.rb
@@ -3,9 +3,6 @@ require 'active_support/concern'
 module Error
   extend ActiveSupport::Concern
   included do
-    rescue_from ActiveRecord::RecordNotFound do |exception|
-      json_response({ message: exception.message }, :not_found)
-    end
     rescue_from ActiveRecord::RecordInvalid do |exception|
       json_response({ message: exception.message }, :not_acceptable)
     end
