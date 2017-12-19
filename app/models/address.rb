7548c5802a150b0bd4fe1cf982fbcbc0ca276288
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-19 10:18:59 -0800

    add risk checker method that returns risk

--- app/models/address.rb
+++ app/models/address.rb
@@ -9,4 +9,15 @@ class Address < ApplicationRecord
   validates :risk, :presence => true
 
   validates_uniqueness_of :street, :scope => [:city, :state, :zip]
+
+  def risk_checker
+    if self.risk == 0 || ((self.risk.to_f / self.orders_checked.to_f) < 0.05)
+      "Low-Risk"
+    elsif (self.risk.to_f / self.orders_checked.to_f) >= 0.05 && (self.risk.to_f / self.orders_checked.to_f) <= 0.1
+      "Medium-Risk"
+    elsif (self.risk.to_f / self.orders_checked.to_f) > 0.1
+      "High-Risk"
+    end
+  end
+
 end
