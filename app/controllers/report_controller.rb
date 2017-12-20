b006362cf65f07a188bcc7c0b655c0bf70ffb355
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-20 14:03:56 -0800

    fix report controller to take proper params for address

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -3,7 +3,7 @@ class ReportController < ApplicationController
     standardize(checks_params)
     if @address = Address.where(street: checks_params[:street], city: checks_params[:city], state: checks_params[:state], zip: checks_params[:zip]).first
     else
-      @address = Address.create!(street: reports_params[:street], city: reports_params[:city], state: reports_params[:state], zip: reports_params[:zip])
+      @address = Address.create!(street: checks_params[:street], city: checks_params[:city], state: checks_params[:state], zip: checks_params[:zip])
     end
     @report = Report.create!(name: reports_params[:name], date: reports_params[:date], carrier: reports_params[:carrier], notes: reports_params[:notes], code: reports_params[:code], price: reports_params[:price], address_id: @address.id)
     @report.risk_update
@@ -12,7 +12,7 @@ class ReportController < ApplicationController
     @address.reports.each do |report|
       @reports.push(name: report.name,date: report.date,carrier: report.carrier,notes: report.notes,code: report.code,price: report.price)
     end
-    render status: 200, json: { message: "Your Report Has been Submitted. Thank You.",
+    render status: 201, json: { message: "Your Report Has been Submitted. Thank You.",
                                 id: @report.id,
                                 safety_level: @safety,
                                 street: @address.street,
